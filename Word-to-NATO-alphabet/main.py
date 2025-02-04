2538eada5881c625526f0c84d9b06f9e062404c6
Nextoni <138438390+Nextoni@users.noreply.github.com>
2025-02-04 11:01:07 +0200

    Add files via upload

--- Word-to-NATO-alphabet/main.py
+++ Word-to-NATO-alphabet/main.py
@@ -4,7 +4,17 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 alphabet = {row.letter: row.code  for index, row in df.iterrows()}
 
-word = input("Enter a word: ").upper()
-transformed_word = [alphabet[let] for let in word]
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
 
-print(transformed_word)
+    try:
+        transformed_word = [alphabet[let] for let in word]
+
+    except KeyError:
+        print("Sorry! Only letters are allowed!")
+        generate_phonetic()
+
+    else:
+        print(transformed_word)
+
+generate_phonetic()
