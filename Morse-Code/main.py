ad2aaaff3c85bb88c68b1b1fda69a210a1f25dd5
Nextoni <138438390+Nextoni@users.noreply.github.com>
2025-05-06 11:15:28 +0300

    Add files via upload

---
+++ Morse-Code/main.py
@@ -0,0 +1,15 @@
+morse_alphabet = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..', ' ': ' ', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
+reverse_morse_alphabet = {'.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f', '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l', '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r', '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x', '-.--': 'y', '--..': 'z', ' ': ' ', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0'}
+
+sentence = input("Enter your sentence: ").lower()
+morse_sentence = ""
+
+if all(char in ['.', '-', ' ', '/'] for char in sentence.strip()):
+    split_sentence = sentence.split("/")[:-1]
+    for char in split_sentence:
+        morse_sentence += reverse_morse_alphabet[char]
+    print(f"english sentence: {morse_sentence}")
+else:
+    for char in sentence:
+        morse_sentence += morse_alphabet[char] + "/"
+    print(f"morse sentence: {morse_sentence}")
